176202b737ca7dfc36e628bf047f7c08afd48466
AmolKumbharCoreCard <161825346+askumbhar@users.noreply.github.com>
2026-03-11 21:37:31 +0530

    update hubconnent signalr

--- web/track-pulse/src/App.tsx
+++ web/track-pulse/src/App.tsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  hubconnection  from './hubConnect'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const connection = async () => {
+    await hubconnection.invoke("JoinRace", "1");
+  }
+ connection();
   return (
     <>
       <div>
